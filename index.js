b1b5d77c5bf0ecb6047d5a5c7946f696d4e248d8
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2024-03-11 22:27:11 +0100

    Feat: add basic pages

---
+++ index.js
@@ -0,0 +1,60 @@
+var swiper = new Swiper(".team-swiper", {
+    navigation: {
+      nextEl: ".swiper-button-nexts",
+      prevEl: ".swiper-button-prevs",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // Responsive breakpoints
+    breakpoints: {
+      250: {
+        slidesPerView: 1.2,
+        spaceBetween: 80
+      },
+      // when window width is &gt;= 320px
+      300: {
+        slidesPerView: 1.2,
+        spaceBetween: 0
+      },
+      // when window width is &gt;= 480px
+      400: {
+        slidesPerView: 1.5,
+        spaceBetween: 20
+      },
+      // when window width is &gt;= 640px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      840: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      },
+      1150: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    }
+  });
+
+
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:3000, // Verlaag de timeout naar 3000 (3 seconden)
+        autoplayHoverPause:true,
+        items:1,
+        animateOut: 'fadeOut'
+    });
+});
+
+
+  
